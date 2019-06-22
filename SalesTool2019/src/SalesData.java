4e594c3c78311f68b075691d0ac175d2559d86bb
Murray Jones <mjones3681@yahoo.comgit config --global user.email mjones3681@yahoo.comgit config --global --list>
2019-06-21 21:02:59 -0500

    Initial commit of project

---
+++ SalesTool2019/src/SalesData.java
@@ -0,0 +1,14 @@
+/* T.J*
+ * Git demo
+ */
+
+public class SalesData {
+	private int data[] = {7, 38, 63};
+   
+  public void display() {
+	  System.out.println("TODO: Implement data display");
+	  
+  }
+}
+
+
